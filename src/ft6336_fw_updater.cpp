55a5d8153cf79b8526c562817c7b4527fc1e9c91
Perun <pierre.schwarz@gmx.de>
2022-03-09 14:20:39 +0100

    update example

---
+++ src/ft6336_fw_updater.cpp
@@ -0,0 +1,190 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+static constexpr int i2c_addr = 0x38;
+static constexpr int i2c_freq = 400000;
+
+#include "ft6336_fw_app_v17.h"
+#define OLD_FIRMWARE_VERSION 16
+
+#define FT6336U_ID         0x64
+#define ID_G_CIPHER        0xa3
+#define ID_G_FIRMID        0xa6
+#define FT_REG_RESET_FW    0x07
+#define FT_ERASE_APP_REG   0x61
+#define FT_READ_ID_REG     0x90
+#define FT_FW_START_REG    0xbf
+#define FT_RST_CMD_REG2    0xbc
+#define FT_UPGRADE_AA      0xAA
+#define FT_UPGRADE_55      0x55
+#define FT_FW_PKT_LEN       128
+#define FT_FW_PKT_DLY_MS     10
+
+#define DELAY_AA      100
+#define DELAY_55       30
+#define DELAY_READID  100
+
+#define UPGRADE_ID_1  0x79
+#define UPGRADE_ID_2  0x1C
+
+#define LOG(fmt, arg...) ESP_LOGI("FT6336_FW", "[%s]: " fmt , __func__ , ## arg)
+#define ERR(fmt, arg...) ESP_LOGE("FT6336_FW", "[%s]: " fmt , __func__ , ## arg)
+#ifndef DEBUG
+#define DBG(fmt, arg...) {}
+#else
+#define DBG(fmt, arg...) ESP_LOGD("FT6336_FW", "[%s]: " fmt "\n" , __func__ , ## arg)
+#endif
+
+static bool i2c_read(const uint8_t *wrbuf, int wrlen, uint8_t *rdbuf, int rdlen)
+{
+  if (wrlen)
+  {
+    if (i2c_err_t::I2C_ERROR_CONTINUE != Wire1.writeTransmission(i2c_addr, const_cast<uint8_t*>(wrbuf), wrlen, false))
+    {
+      ERR("I2C write error");
+      return false;
+    }
+  }
+  if (i2c_err_t::I2C_ERROR_OK != Wire1.readTransmission(i2c_addr, rdbuf, rdlen, true))
+  {
+    ERR("I2C read error");
+    return false;
+  }
+  return true;
+}
+
+static bool i2c_write(const uint8_t *buf, int len)
+{
+  if (i2c_err_t::I2C_ERROR_OK != Wire1.writeTransmission(i2c_addr, const_cast<uint8_t*>(buf), len, true))
+  {
+    ERR("I2C write error");
+    return false;
+  }
+  return true;
+}
+
+static bool read_id(void)
+{
+  uint8_t reg_val[2] = {0};
+  uint8_t packet_buf[4];
+
+  delay(DELAY_READID);
+  packet_buf[0] = FT_READ_ID_REG;
+  packet_buf[1] = packet_buf[2] = packet_buf[3] = 0x00;
+
+  i2c_read(packet_buf, 4, reg_val, 2);
+  if (reg_val[0] != UPGRADE_ID_1
+   || reg_val[1] != UPGRADE_ID_2) {
+    ERR("READ-ID not ok: %x %x", reg_val[0], reg_val[1]);
+    return false;
+  }
+
+  return true;
+}
+
+static void ft6x36_fw_upgrade(const uint8_t *data, uint32_t data_len)
+{
+  int i;
+  uint8_t buf[6 + FT_FW_PKT_LEN];
+
+  size_t retry = 16;
+  do
+  {
+    LOG("Reset CTPM");
+    buf[0] = FT_RST_CMD_REG2;
+    buf[1] = FT_UPGRADE_AA;
+    i2c_write(buf, 2);
+    delay(DELAY_AA);
+    buf[1] = FT_UPGRADE_55;
+    i2c_write(buf, 2);
+    delay(DELAY_55);
+
+    LOG("Enter upgrade mode");
+    buf[0] = FT_UPGRADE_55;
+    buf[1] = FT_UPGRADE_AA;
+    if (!i2c_write(buf, 2))
+    {
+      ERR("failed to enter upgrade mode");
+      continue;
+    }
+    LOG("Check READ-ID");
+  } while (!read_id() && --retry);
+
+  if (retry == 0)
+  {
+    return;
+  }
+
+  LOG("Erase current app");
+  buf[0] = FT_ERASE_APP_REG;
+  if (!i2c_write(buf, 1))
+  {
+    ERR("Couldn't Erase");
+    return;
+  }
+
+  LOG("Write firmware to CTPM flash");
+  buf[0] = FT_FW_START_REG;
+  buf[1] = 0x00;
+  bool fail = false;
+  for (i = 0; i < data_len; i += FT_FW_PKT_LEN) {
+    uint32_t length = data_len - i;
+    if (length > FT_FW_PKT_LEN) { length = FT_FW_PKT_LEN; }
+
+    buf[2] = (uint8_t) (i >> 8);
+    buf[3] = (uint8_t) i;
+    buf[4] = (uint8_t) (length >> 8);
+    buf[5] = (uint8_t) length;
+    memcpy(&buf[6], &data[i], length);
+    if (!i2c_write(buf, length + 6))
+    {
+      fail = true;
+      ERR("Couldn't Write");
+      break;
+    }
+    delay(FT_FW_PKT_DLY_MS);  
+  }
+  if (fail)
+  {
+    ERR("Failed to flash FW");
+  }
+  else
+  {
+    LOG("Success to flash FW");
+  }
+  delay(50);
+
+  buf[0] = FT_REG_RESET_FW;
+  i2c_write(buf, 1);
+  delay(1000);
+}
+
+void ft6336_fw_updater(void)
+{
+  uint8_t wbuf, rbuf;
+
+  wbuf = ID_G_CIPHER;
+  size_t retry = 32;
+  do
+  {
+    if (i2c_read(&wbuf, 1, &rbuf, 1) && rbuf == FT6336U_ID) { break; }
+    LOG("wait CTPM response");
+    delay(128);
+  } while (--retry);
+
+  /* Get current firmware version */
+  uint8_t fw_ver = 0;
+  {
+    wbuf = ID_G_FIRMID;
+    i2c_read(&wbuf, 1, &fw_ver, 1);
+    LOG("FT6336U firmware version: %d.0.0", fw_ver);
+    String log = "FT6336U firmware version: " + String(fw_ver) + ".0.0";
+    Serial.println(log);
+  }
+
+  if (fw_ver == 0 || fw_ver == OLD_FIRMWARE_VERSION)
+  {
+    LOG("FW length is %ld", sizeof(ft6336_fw_app));
+    ft6x36_fw_upgrade(ft6336_fw_app, sizeof(ft6336_fw_app));
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,195 @@
 #include <Arduino.h>
-#include <Wire.h>
+#include <driver/i2s.h>
+#include "Wire.h"			  
+#include "Config.h"
+#include "MPU6886.h"
+#include "AXP192.h"
+#include "RTC.h"
+#include "SPI.h"
+#include "ILI9341_Defines.h"
 
-#define SDA1 21
-#define SCL1 22
+#define CONFIG_I2S_BCK_PIN 12
+#define CONFIG_I2S_LRCK_PIN 0
+#define CONFIG_I2S_DATA_PIN 2
+#define CONFIG_I2S_DATA_IN_PIN 34
 
- #define CST_DEVICE_ADDR 0x38
+#define CST_DEVICE_ADDR 0x38
+#define CST_INT 39
 
-TwoWire I2Cone = TwoWire(0);
+#define DEFAULT_INTERVAL 13
+#define SPI_FREQUENCY  40000000
+#define TFT_SPI_MODE SPI_MODE0
 
-uint8_t ft6336(uint8_t reg) {
-  I2Cone.beginTransmission((uint8_t)CST_DEVICE_ADDR);
-  I2Cone.write(reg);
-  I2Cone.endTransmission();
-  I2Cone.requestFrom((uint8_t)CST_DEVICE_ADDR, uint8_t(1));
-  return I2Cone.read();
+#define TFT_MISO 38
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   5      // Chip select control pin
+#define TFT_DC   15     // Data Command control pin
+
+#define TFT_SWRST   0x01
+#define TFT_INVON   0x21
+
+#define CS_L GPIO.out_w1tc = (1 << TFT_CS);GPIO.out_w1tc = (1 << TFT_CS)
+#define CS_H GPIO.out_w1ts = (1 << TFT_CS)//;GPIO.out_w1ts = (1 << TFT_CS)
+
+#define DC_C GPIO.out_w1tc = (1 << TFT_DC)//;GPIO.out_w1tc = (1 << TFT_DC)
+#define DC_D GPIO.out_w1ts = (1 << TFT_DC)//;GPIO.out_w1ts = (1 << TFT_DC)
+
+void ft6336_fw_updater(void);
+
+typedef enum {
+    kPOWER_EXTERNAL = 0,
+    kPOWER_INTERNAL,
+    kPOWER_MAX
+}system_power_t;
+
+// Power
+AXP192 Axp;
+
+MPU6886 IMU;
+
+RTC  Rtc;
+
+SPIClass& spi = SPI;
+
+
+void spi_begin(void)
+{
+ spi.beginTransaction(SPISettings(SPI_FREQUENCY, MSBFIRST, TFT_SPI_MODE)); 
+ CS_L;
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+void spi_end(void)
+{   
+    CS_H;
+    spi.endTransaction();
+}
+
+void writecommand(uint8_t c)
+{
+  spi_begin(); // CS_L;
+  DC_C;  
+  spi.transfer(c);
+  DC_D;
+  spi_end();  // CS_H;
+}
+
+void writedata(uint8_t d)
+{
+  spi_begin(); // CS_L;
+  DC_D;        // Play safe, but should already be in data mode  
+  spi.transfer(d);
+  CS_L;        // Allow more hold time for low VDI rail
+  spi_end();   // CS_H;
+}
+
+
+
+void TFT_eSPI__init(uint8_t tc)
+{  
+  spi.begin(TFT_SCLK, TFT_MISO, TFT_MOSI, -1);
+
+  // Set to output once again in case D6 (MISO) is used for CS
+  digitalWrite(TFT_CS, HIGH); // Chip select high (inactive)
+  pinMode(TFT_CS, OUTPUT);
+ 
+  digitalWrite(TFT_DC, HIGH); // Data/Command high = data mode
+  pinMode(TFT_DC, OUTPUT);
+  spi_end();
+
+
+  // Toggle RST low to reset
+  writecommand(TFT_SWRST); // Software reset
+ 
+  delay(150); // Wait for reset to complete
+
+  // This loads the driver specific initialisation code  <<<<<<<<<<<<<<<<<<<<< ADD NEW DRIVERS TO THE LIST HERE <<<<<<<<<<<<<<<<<<<<<<<
+
+  #include "ILI9341_Init.h"
+  
+  writecommand(TFT_INVON);
+}
+
+uint8_t ft6336(uint8_t reg) 
+{
+  Wire1.beginTransmission((uint8_t)CST_DEVICE_ADDR);
+  Wire1.write(reg);
+  Wire1.endTransmission();
+  Wire1.requestFrom((uint8_t)CST_DEVICE_ADDR, uint8_t(1));
+  return Wire1.read();
+}
+
+void ft6336(uint8_t reg, uint8_t value) 
+{
+  Wire1.beginTransmission(CST_DEVICE_ADDR);
+  Wire1.write(reg);
+  Wire1.write((uint8_t)value);
+  Wire1.endTransmission();
+}
+
+uint8_t interval(uint8_t ivl) 
+{
+  ft6336(0x88, ivl);
+  return ft6336(0x88);
+}
+
+void M5Touch__begin() 
+{
+  Wire1.begin(21, 22);
+  pinMode(CST_INT, INPUT);
+
+  // By default, the FT6336 will pulse the INT line for every touch
+  // event. But because it shares the Wire1 TwoWire/I2C with other
+  // devices, we cannot easily create an interrupt service routine to
+  // handle these events. So instead, we set the INT wire to polled mode,
+  // so it simply goes low as long as there is at least one valid touch.
+  ft6336(0xA4, 0x00);
+
+  Serial.print("touch: ");
+  if (interval(DEFAULT_INTERVAL) == DEFAULT_INTERVAL) 
+  {
+    Serial.printf("FT6336 ready (fw id 0x%02X rel %d, lib 0x%02X%02X)\n",
+                  ft6336(0xA6), ft6336(0xAF), ft6336(0xA1), ft6336(0xA2));
+  } 
+  else 
+  {
+    Serial.println("ERROR - FT6336 not responding");
+  }
+}
+
+
+void M5Core2_begin() 
+{  
+  // UART
   Serial.begin(115200);
-  // fast I2C spped
-  // I2Cone.begin(SDA1,SCL1,400000); // SDA pin 21, SCL pin 22 TTGO TQ
-  // slow I2C spped
-  I2Cone.begin(SDA1,SCL1,100000); // SDA pin 21, SCL pin 22 TTGO TQ
-  Serial.println("setup done");
+  Serial.flush();
+  delay(50);
+  Serial.print("M5Core2 initializing...");
+
+  // I2C init
+  Wire.begin(32, 33);
+
+  Axp.begin(kMBusModeOutput);
+
+  ft6336_fw_updater();  
+
+  // LCD INIT
+  TFT_eSPI__init(0);
+
+  // Touch init
+  M5Touch__begin(); // Touch begin after AXP begin. (Reset at the start of AXP)
+
+  Rtc.begin();
 }
 
-void loop() { 
-  Serial.println("scan");
-  Serial.println(ft6336(0xA3));
-  Serial.println();
-  delay(5000);
+void setup()
+{   
+    M5Core2_begin();    
+}
+
+void loop()
+{
+    delay(1000);
+    Serial.println("read device id: ");
+    Serial.println(ft6336(0xA3));
 }
